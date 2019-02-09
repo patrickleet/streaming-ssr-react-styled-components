792da703e246726f63b04390588251dbccbfe910
Patrick Scott <pat@patscott.io>
2019-02-08 19:07:15 -0500

    fix: remove unused code for parsing template

--- server/lib/client.js
+++ server/lib/client.js
@@ -1,20 +1,8 @@
 import fs from 'fs'
 import path from 'path'
-import cheerio from 'cheerio'
 
-export const htmlPath = path.join(process.cwd(), 'dist', 'client', 'index.html')
-export const rawHTML = fs.readFileSync(htmlPath).toString()
-
-export const parseRawHTMLForData = (template, selector = '#js-entrypoint') => {
-  const $template = cheerio.load(template)
-  let src = $template(selector).attr('src')
-
-  return {
-    src
-  }
-}
-
-const clientData = parseRawHTMLForData(rawHTML)
+const htmlPath = path.join(process.cwd(), 'dist', 'client', 'index.html')
+const rawHTML = fs.readFileSync(htmlPath).toString()
 
 const appString = '<div id="app">'
 const splitter = '###SPLIT###'
